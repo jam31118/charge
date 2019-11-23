7f8325156f3cb1ed0bc12fb61f6d0b21c73b492f
Ahn, Seongjin <jam45@naver.com>
2019-11-23 22:59:15 +0900

    clean-up and optimization

--- charge/time.py
+++ charge/time.py
@@ -3,33 +3,33 @@
 import numpy as np
 from scipy.optimize import root, root_scalar
 
+
 def tr(r, t, rs, c):
     """Evaluate retarded time in atomic unit"""
-    def g(dt, r, t, rs, c): return np.sqrt(np.square(r-rs(t-dt)).sum(-1)) - c*dt
-    _dt_trial = 0.0
-    _dt = None
-    if abs(g(_dt_trial, r, t, rs, c)) < 1e-15: _dt = _dt_trial
-    else:
-        _dt_trial = np.sqrt(np.square(r-rs(t)).sum(-1)) / c 
-        _root_results = root_scalar(g, args=(r,t,rs,c), method='secant', x0=0.0, x1=_dt_trial)
-        #_result = root(g, _dt_trial, args=(r,t,rs,c))
-        #try: _dt, = fsolve(g, 0.0, args=(r,t,rs,c))
-        #except: raise Exception("Failed to find retarded time")
 
-#        if not _result.success:
-#            print("_dt_trial: {}".format(_dt_trial))
-#            _mesg = "Failed to find retarded time with 'OptimizeResult':\n{}"
-#            raise Exception(_mesg.format(_result))
-#        _dt, = _result.x
-        
-        if not _root_results.converged:
-            _mesg = "Failed to find retarded time with 'RootResults':\n{}"
-            raise Exception(_mesg.format(_root_results))
-        _dt = _root_results.root
+    ## Define objective function
+    def g(dt,r,t,rs,c): return np.sqrt(np.square(r-rs(t-dt)).sum(-1)) - c*dt
 
+    ## Find root, which is the retarded amount of time
 
-    assert _dt is not None
+    # Set initial two points for secant method
+    _dt0, _dt1 = 0.0, np.sqrt(np.square(r-rs(t)).sum(-1)) / c 
+
+    # Find root
+    _root_results = root_scalar(
+            g, args=(r,t,rs,c), method='secant', x0=_dt0, x1=_dt1)
+    
+    # Exception handling
+    if not _root_results.converged:
+        _mesg = "Failed to find retarded time with 'RootResults':\n{}"
+        raise Exception(_mesg.format(_root_results))
+
+    # Finalize for the retarded amount of time `_dt`
+    _dt = _root_results.root
+
+    # Return the retarded time `tr` = `t - _dt`
     return t - _dt
+
 
 def tr_arr(r_arr, t, rs, c):
     """Evaluate retarded time for each position vector given in atomic unit"""
