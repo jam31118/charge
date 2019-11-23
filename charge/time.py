1e689119bf92fb205726de38daf739494280a25c
Ahn, Seongjin <jam45@naver.com>
2019-11-23 22:45:53 +0900

    improve stability to find retarded time

--- charge/time.py
+++ charge/time.py
@@ -1,13 +1,34 @@
 """Routine for retarded time"""
 
 import numpy as np
-from scipy.optimize import fsolve
+from scipy.optimize import root, root_scalar
 
 def tr(r, t, rs, c):
     """Evaluate retarded time in atomic unit"""
     def g(dt, r, t, rs, c): return np.sqrt(np.square(r-rs(t-dt)).sum(-1)) - c*dt
-    try: _dt, = fsolve(g, 0.0, args=(r,t,rs,c))
-    except: raise Exception("Failed to find retarded time")
+    _dt_trial = 0.0
+    _dt = None
+    if abs(g(_dt_trial, r, t, rs, c)) < 1e-15: _dt = _dt_trial
+    else:
+        _dt_trial = np.sqrt(np.square(r-rs(t)).sum(-1)) / c 
+        _root_results = root_scalar(g, args=(r,t,rs,c), method='secant', x0=0.0, x1=_dt_trial)
+        #_result = root(g, _dt_trial, args=(r,t,rs,c))
+        #try: _dt, = fsolve(g, 0.0, args=(r,t,rs,c))
+        #except: raise Exception("Failed to find retarded time")
+
+#        if not _result.success:
+#            print("_dt_trial: {}".format(_dt_trial))
+#            _mesg = "Failed to find retarded time with 'OptimizeResult':\n{}"
+#            raise Exception(_mesg.format(_result))
+#        _dt, = _result.x
+        
+        if not _root_results.converged:
+            _mesg = "Failed to find retarded time with 'RootResults':\n{}"
+            raise Exception(_mesg.format(_root_results))
+        _dt = _root_results.root
+
+
+    assert _dt is not None
     return t - _dt
 
 def tr_arr(r_arr, t, rs, c):
